4688426791cdf3ca128c650da53db334e4c294a9
Peter Fortune <pfortune@gmail.com>
2024-03-16 23:00:51 +0000

    Add Todo form

--- todo-svelte/src/App.svelte
+++ todo-svelte/src/App.svelte
@@ -1,47 +1,22 @@
+<div class="section box">
+  <div class="field is-horizontal">
+    <div class="field-label is-normal">
+      <label for="todo" class="label">What should I do?</label>
+    </div>
+    <div class="field-body">
+      <div class="field">
+        <p class="control">
+          <input bind:value={todoText} id="todo" class="input" type="text" placeholder="Type something...">
+        </p>
+      </div>
+      <button on:click={addTodo} class="button">Add Todo</button>
+    </div>
+  </div>
+</div>
+
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  let todoText;
+  function addTodo() {
+    console.log(todoText)
+  }
 </script>
-
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
