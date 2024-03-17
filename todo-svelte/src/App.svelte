a31b1a87167adae740e48c0428fc84541d738d26
Peter Fortune <pfortune@gmail.com>
2024-03-17 20:38:49 +0000

    Added Title component

--- todo-svelte/src/App.svelte
+++ todo-svelte/src/App.svelte
@@ -1,66 +1,7 @@
-<div class="container">
-  <div class="box has-text-centered">
-    <div class="title"> Simple Todo List</div>
-    <div class="subtitle">Fun things to do</div>
-  </div>
-</div>
-
-<div class="section box">
-  <div class="field is-horizontal">
-    <div class="field-label is-normal">
-      <label for="todo" class="label">What should I do?</label>
-    </div>
-    <div class="field-body">
-      <div class="field">
-        <p class="control">
-          <input bind:value={todoText} id="todo" class="input" type="text" on:keydown={handleKeyDown} placeholder="Type something...">
-        </p>
-      </div>
-      <button on:click={addTodo}  class="button">Add Todo</button>
-    </div>
-  </div>
-</div>
-
-<div class="section box">
-  <div class="title is-6">Things yet do</div>
-  <table class="table is-fullwidth">
-    <thead>
-      <th>Task</th>
-      <th>Date</th>
-      <th></th>
-    </thead>
-    <tbody>
-      {#each todoItems as todo}
-        <tr>
-          <td> {todo.text} </td>
-          <td> {todo.date} </td>
-          <td><button on:click={() => deleteTodo(todo.id)} class="button">complete</button></td>
-        </tr>
-      {/each}
-    </tbody>
-  </table>
-</div>
-
-<div class="section box">
-  <div class="title is-6">Things done</div>
-  <table id="done-table" class="table is-fullwidth">
-    <thead>
-      <th>Task</th>
-      <th>Date</th>
-      <th></th>
-    </thead>
-    <tbody>
-      {#each doneItems as todo}
-        <tr>
-          <td> {todo.text} </td>
-          <td> {todo.date}</td>
-        </tr>
-      {/each}
-    </tbody>
-  </table>
-</div>
-
 <script>
+  import { v4 as uuidv4 } from 'uuid';
+  import Title from './Title.svelte';
+
   let todoText = "";
   let todoItems = [];
   let todo = {};
@@ -68,7 +9,7 @@
 
   function addTodo() {
     todo = {
-      id: todoItems.length + 1,
+      id: uuidv4(),
       text: todoText,
       date: new Date().toLocaleString("en-IE"),
     }
@@ -87,7 +28,6 @@
       todoItems = [...todoItems];
       doneItems = [...doneItems];
     }
-    
   }
 
   function handleKeyDown(event) {
@@ -97,3 +37,63 @@
     }
   }
 </script>
+
+<div class="container">
+  <Title title="Simple Todo List" subTitle="Fun Things To Do"/>
+
+  <div class="section box">
+    <div class="field is-horizontal">
+      <div class="field-label is-normal">
+        <label for="todo" class="label">What should I do?</label>
+      </div>
+      <div class="field-body">
+        <div class="field">
+          <p class="control">
+            <input bind:value={todoText} id="todo" class="input" type="text" on:keydown={handleKeyDown} placeholder="Type something...">
+          </p>
+        </div>
+        <button on:click={addTodo}  class="button">Add Todo</button>
+      </div>
+    </div>
+  </div>
+
+  <div class="section box">
+    <div class="title is-6">Things yet to do</div>
+    <table class="table is-fullwidth">
+      <thead>
+        <th>Task</th>
+        <th>Date</th>
+        <th></th>
+      </thead>
+      <tbody>
+        {#each todoItems as todo}
+          <tr>
+            <td> {todo.text} </td>
+            <td> {todo.date} </td>
+            <td><button on:click={() => deleteTodo(todo.id)} class="button">complete</button></td>
+          </tr>
+        {/each}
+      </tbody>
+    </table>
+  </div>
+
+  <div class="section box">
+    <div class="title is-6">Things done</div>
+    <table id="done-table" class="table is-fullwidth">
+      <thead>
+        <th>Task</th>
+        <th>Date</th>
+        <th></th>
+      </thead>
+      <tbody>
+        {#each doneItems as todo}
+          <tr>
+            <td> {todo.text} </td>
+            <td> {todo.date}</td>
+          </tr>
+        {/each}
+      </tbody>
+    </table>
+  </div>
+
+</div>
